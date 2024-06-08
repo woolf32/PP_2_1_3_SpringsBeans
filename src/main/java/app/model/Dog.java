2d7b911e1f2b20bc1334d9ea82ae9bcefd532248
йй <woolf_32@mail.ru>
2024-06-08 14:39:21 +0300

    add class dog

--- src/main/java/app/model/Dog.java
+++ src/main/java/app/model/Dog.java
@@ -1,8 +1,20 @@
 package app.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@Component
+@Component("dogBean")
 public class Dog extends Animal {
+    private Long timer;
+
+    @Override
+    public String toString() {
+        return "Im a Dog";
+    }
+
+
+
+
+
 
 }
