0b6adbd90798d099b770f28b6ef2ed53cf08f515
йй <woolf_32@mail.ru>
2024-06-08 14:40:02 +0300

    add component id

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,8 +1,13 @@
 package app.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component("timerBean")
 public class Timer {
 
     private Long nanoTime = System.nanoTime();
+
 
     public Long getTime() {
         return nanoTime;
